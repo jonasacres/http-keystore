db1cf2877613853d772bd443391880fcb84c78fb
Jonas Acres <acresjonas@gmail.com>
2017-06-08 08:40:10 -0700

    push changes to websockets

--- http-keystore.rb
+++ http-keystore.rb
@@ -10,6 +10,8 @@ set :sockets, []
 set :port, 11000
 set :bind, "0.0.0.0"
 
+@sockets_by_key = {}
+
 def keys_dir
   "keys"
 end
@@ -18,13 +20,26 @@ def path_for_key(key)
   "#{keys_dir}/#{key}"
 end
 
-def send_key(key, ws)
+def register_socket(key, ws)
+  @sockets_by_key[key] ||= []
+  @sockets_by_key[key] << ws
+end
+
+def unregister_socket(key, ws)
+  @sockets_by_key[key].delete(ws)
+end
+
+def send_key_to_socket(key, ws)
   path = path_for_key(key)
   if File.exist?(path) then
     ws.send({"Last-Modified" => File.mtime(path).to_s, "data" => File.read(path)}.to_json)
   else
     ws.send({"Last-Modified" => nil, "data" => nil}.to_json)
   end
+end
+
+def send_key(key)
+  (@sockets_by_key[key] || []).each { |ws| send_key_to_socket(key, ws) }
 end
 
 get '/:key' do |key|
@@ -34,15 +49,17 @@ get '/:key' do |key|
   if request.websocket? then
     request.websocket do |ws|
       ws.onopen do
-        send_key(key, ws)
+        register_socket(key, ws)
+        send_key_to_socket(key, ws)
         settings.sockets << ws
       end
 
       ws.onmessage do |msg|
-        send_key(key, ws)
+        send_key_to_socket(key, ws)
       end
 
       ws.onclose do
+        unregister_socket(key, ws)
         settings.sockets.delete(ws)
       end
     end
@@ -58,6 +75,9 @@ post '/:key' do |key|
   request.body.rewind
   Dir.mkdir(keys_dir) unless File.exist?(keys_dir)
   IO.write(path_for_key(key), request.body.read)
+
+  send_key(key)
+
   [ 200, {}, "" ]
 end
 
@@ -66,5 +86,6 @@ delete '/:key' do |key|
   halt 404 unless File.exist?(path_for_key(key))
 
   File.unlink(path_for_key(key))
+  send_key(key)
 end
 
